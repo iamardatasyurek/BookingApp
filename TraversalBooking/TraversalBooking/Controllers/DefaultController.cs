8005c6f0b5746ebb87fc4d78557792e4b255f4a3
Arda Taşyürek <ardatasyurek@outlook.com>
2022-09-29 00:21:54 +0300

    Added BusinessLayer and free template

---
+++ TraversalBooking/TraversalBooking/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalBooking.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
