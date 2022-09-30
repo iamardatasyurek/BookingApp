382bab4d4f8e006a9dd471155c7f88241c55bd62
Arda Taşyürek <ardatasyurek@outlook.com>
2022-09-30 14:59:41 +0300

    Completed home page codes

---
+++ TraversalBooking/TraversalBooking/ViewComponents/Default/_Feature.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalBooking.ViewComponents.Default
+{
+    public class _Feature : ViewComponent
+    {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+        public IViewComponentResult Invoke()
+        {
+            //ViewBag.image1 = featureManager.
+            return View();
+        }
+    }
+}
---
+++ TraversalBooking/TraversalBooking/ViewComponents/Default/_SliderPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace TraversalBooking.ViewComponents.Default
+{
+    public class _SliderPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
